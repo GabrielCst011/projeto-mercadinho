d044239f1c921e488195e72b42b2e5963acfa77c
Gabriel Costa <bbbgabriel35@outlook.com>
2025-08-07 21:21:25 -0300

    Atualiza config.py com novas variáveis

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://admin:Gjwe4wYI6jHiYTL0vggfTMPHCqadGAV0@dpg-d2ajvvndiees73ds8gr0-a.oregon-postgres.render.com/db_mercadinho")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
