ace7900860260cc5649e4ac8c3e183e20a01a3c3
Gabriel Costa <bbbgabriel35@outlook.com>
2025-08-11 20:46:36 -0300

    Update Config

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev_key")  # chave default para local
-    ADMIN_CPF = os.getenv("ADMIN_CPF", "")
-    
-    uri = os.getenv("DATABASE_URL")
-    if uri and uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-    SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
